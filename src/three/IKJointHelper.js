ecba0792358a251eb36b64beb489f1df10bbb988
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-10-19 23:18:17 -0700

    Add rotation limit vis -- related to #11

--- src/three/IKJointHelper.js
+++ src/three/IKJointHelper.js
@@ -6,6 +6,64 @@ import { HALF_PI } from '../core/utils/constants.js';
 
 const tempPos = new Vector3();
 const tempRot = new Vector3();
+
+class RotationLimitHelper extends Mesh {
+
+	constructor( material, dof ) {
+
+		super( undefined, material );
+		this._dof = dof;
+		this._min = null;
+		this._delta = null;
+		this.setLimits( 0, 2 * Math.PI );
+
+
+	}
+
+	setLimits( min, max ) {
+
+		const delta = Math.min( max - min, 2 * Math.PI );
+		if ( min === - Infinity ) {
+
+			min = 0;
+
+		}
+		if ( this._min === min && this._delta === delta ) {
+
+			return;
+
+		}
+
+		this._min = min;
+		this._delta = delta;
+
+		if ( this.geometry ) {
+
+			this.geometry.dispose();
+
+		}
+
+		const dof = this._dof;
+		const geometry = new CylinderBufferGeometry( 0.075, 0.075, 1e-7, 100, 1, false, min, delta );
+
+		if ( dof === DOF.EX ) {
+
+			geometry.rotateZ( HALF_PI );
+
+		}
+
+		if ( dof === DOF.EZ ) {
+
+			geometry.rotateX( HALF_PI );
+
+		}
+
+		this.geometry = geometry;
+
+	}
+
+}
+
 export class IKJointHelper extends IKLinkHelper {
 
 	constructor( joint ) {
@@ -17,15 +75,31 @@ export class IKJointHelper extends IKLinkHelper {
 			new MeshStandardMaterial(),
 		);
 
+		const xRotationLimits = new RotationLimitHelper(
+			new MeshStandardMaterial(),
+			DOF.EX,
+		);
+
 		const yRotationMesh = new Mesh(
 			new CylinderBufferGeometry( 0.05, 0.05, 0.25, 30, 1 ),
 			new MeshStandardMaterial(),
+		);
+
+		const yRotationLimits = new RotationLimitHelper(
+			new MeshStandardMaterial(),
+			DOF.EY,
 		);
 
 		const zRotationMesh = new Mesh(
 			new CylinderBufferGeometry( 0.05, 0.05, 0.25, 30, 1 ).rotateX( HALF_PI ),
 			new MeshStandardMaterial(),
 		);
+
+		const zRotationLimits = new RotationLimitHelper(
+			new MeshStandardMaterial(),
+			DOF.EZ,
+		);
+		zRotationLimits.rotation.set( HALF_PI, 0, 0 );
 
 		const freeRotationMesh = new Mesh(
 			new SphereBufferGeometry( 0.05, 30, 30 ),
@@ -52,6 +126,9 @@ export class IKJointHelper extends IKLinkHelper {
 			xRotationMesh,
 			yRotationMesh,
 			zRotationMesh,
+			xRotationLimits,
+			yRotationLimits,
+			zRotationLimits,
 			freeRotationMesh,
 			translationMesh,
 			fixedMesh,
@@ -60,6 +137,9 @@ export class IKJointHelper extends IKLinkHelper {
 		this.xRotationMesh = xRotationMesh;
 		this.yRotationMesh = yRotationMesh;
 		this.zRotationMesh = zRotationMesh;
+		this.xRotationLimits = xRotationLimits;
+		this.yRotationLimits = yRotationLimits;
+		this.zRotationLimits = zRotationLimits;
 		this.translationMesh = translationMesh;
 		this.freeRotationMesh = freeRotationMesh;
 		this.fixedMesh = fixedMesh;
@@ -71,6 +151,9 @@ export class IKJointHelper extends IKLinkHelper {
 		this.xRotationMesh.scale.setScalar( s );
 		this.yRotationMesh.scale.setScalar( s );
 		this.zRotationMesh.scale.setScalar( s );
+		this.xRotationLimits.scale.setScalar( s );
+		this.yRotationLimits.scale.setScalar( s );
+		this.zRotationLimits.scale.setScalar( s );
 		this.freeRotationMesh.scale.setScalar( s );
 		this.fixedMesh.scale.setScalar( s );
 
@@ -84,6 +167,9 @@ export class IKJointHelper extends IKLinkHelper {
 			xRotationMesh,
 			yRotationMesh,
 			zRotationMesh,
+			xRotationLimits,
+			yRotationLimits,
+			zRotationLimits,
 			freeRotationMesh,
 			translationMesh,
 			fixedMesh,
@@ -92,6 +178,9 @@ export class IKJointHelper extends IKLinkHelper {
 		xRotationMesh.visible = false;
 		yRotationMesh.visible = false;
 		zRotationMesh.visible = false;
+		xRotationLimits.visible = false;
+		yRotationLimits.visible = false;
+		zRotationLimits.visible = false;
 		freeRotationMesh.visible = false;
 		translationMesh.visible = false;
 		fixedMesh.visible = false;
@@ -105,13 +194,19 @@ export class IKJointHelper extends IKLinkHelper {
 		if ( joint.rotationDoFCount === 3 ) {
 
 			freeRotationMesh.visible = true;
-			window.ikRoot = this;
+			xRotationLimits.visible = true;
+			yRotationLimits.visible = true;
+			zRotationLimits.visible = true;
 
 		} else {
 
 			xRotationMesh.visible = Boolean( joint.dofFlags[ DOF.EX ] );
 			yRotationMesh.visible = Boolean( joint.dofFlags[ DOF.EY ] );
 			zRotationMesh.visible = Boolean( joint.dofFlags[ DOF.EZ ] );
+
+			xRotationLimits.visible = Boolean( joint.dofFlags[ DOF.EX ] );
+			yRotationLimits.visible = Boolean( joint.dofFlags[ DOF.EY ] );
+			zRotationLimits.visible = Boolean( joint.dofFlags[ DOF.EZ ] );
 
 		}
 
@@ -130,6 +225,9 @@ export class IKJointHelper extends IKLinkHelper {
 			xRotationMesh,
 			yRotationMesh,
 			zRotationMesh,
+			xRotationLimits,
+			yRotationLimits,
+			zRotationLimits,
 			freeRotationMesh,
 			translationMesh,
 		} = this;
@@ -150,12 +248,29 @@ export class IKJointHelper extends IKLinkHelper {
 		translationMesh.scale.copy( tempPos );
 
 		xRotationMesh.position.copy( tempPos );
+		xRotationLimits.position.copy( tempPos );
+		xRotationLimits.setLimits(
+			joint.getMinLimit( DOF.EX ),
+			joint.getMaxLimit( DOF.EX ),
+		);
 
 		yRotationMesh.position.copy( tempPos );
 		yRotationMesh.rotation.set( tempRot.x, 0, 0 );
+		yRotationLimits.position.copy( tempPos );
+		yRotationLimits.rotation.set( tempRot.x, 0, 0 );
+		yRotationLimits.setLimits(
+			joint.getMinLimit( DOF.EY ),
+			joint.getMaxLimit( DOF.EY ),
+		);
 
 		zRotationMesh.position.copy( tempPos );
 		zRotationMesh.rotation.set( tempRot.x, tempRot.y, 0 );
+		zRotationLimits.position.copy( tempPos );
+		zRotationLimits.rotation.set( tempRot.x, tempRot.y, 0 );
+		zRotationLimits.setLimits(
+			joint.getMinLimit( DOF.EZ ),
+			joint.getMaxLimit( DOF.EZ ),
+		);
 
 		freeRotationMesh.position.copy( tempPos );
 
